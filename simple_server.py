559774298c7a80bf8c36611a1932d39d9771b5f9
El Dorado <annurag94@gmail.com>
2017-04-07 19:04:58 +0530

    Updated ftpserver which creates directory.

--- simple_server.py
+++ simple_server.py
@@ -1,9 +1,15 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+import os
+
+home_dir = os.path.expanduser("~")
+if not os.path.exists(home_dir + "/skynet_files"):
+    os.makedirs(home_dir + "/skynet_files")
+
 authorizer = DummyAuthorizer()
 # authorizer.add_user("user", "12345", "/home/giampaolo", perm="elradfmw")
-authorizer.add_anonymous("/home/dorado/work/skynet", perm='elradfmwM')
+authorizer.add_anonymous(home_dir + "/skynet_files", perm='elradfmwM')
 handler = FTPHandler
 handler.authorizer = authorizer
 server = FTPServer(("127.0.0.1", 2121), handler)
