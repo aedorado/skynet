20cf9a8c302c2a6d245a0731059862aadad7ec79
Kritika sharma <ks4347630@gmail.com>
2017-06-11 11:43:47 +0530

    servers incorporated

--- persistence/persist.py
+++ persistence/persist.py
@@ -4,32 +4,60 @@ import sqlite3 as db
 
 def start_listening():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('172.19.17.98', 11111))
+	host = socket.gethostname()               # Get local machine name
+	#s.bind(('172.19.17.98', 11111))          # for different machines         
+	s.bind((host, 11113))                     # for same machine
 	s.listen(10)
+
+	stor = Storage()                # database will be created only once
 
 	while True:
 		conn, addr = s.accept()
 		msg = conn.recv(1024)
-		if msg.find('1:JOIN') is not -1:
-			new_ip = msg[msg.rfind(':') + 1:]
-			print new_ip
-			stor = Storage()
-			try:
-				stor.add_new_server(new_ip)
-				conn.send('2:ADDED')
-			except:
-				print 'Error occured'
-				conn.send('3:FAILED')				
-		elif msg.find('3:HBEAT') is not -1:
-			new_ip = msg[msg.rfind(':') + 1:]
-			stor = Storage()
-			try:
-				stor.add_heartbeat(new_ip)
-				conn.send('4:UPDATED')
-			except:
-				print 'Error occured'
-				conn.send('5:FAILED')	
-			pass
+		print msg
+		if msg.find("master") is not -1:
+			if msg.find('1:JOIN') is not -1:
+				new_ip = msg[msg.rfind(':') + 1:]   # finding the ip from the message received
+				print new_ip
+				try:
+					stor.add_new_master(new_ip)     # new server added
+					conn.send('Master ADDED')
+				except:
+					print 'Error occured'
+					conn.send('Master addition FAILED')				
+			elif msg.find('2:HBEAT') is not -1:     
+				new_ip = msg[msg.rfind(':') + 1:]
+				try:
+					stor.add_heartbeat_master(new_ip)      # updating the time when last ping came from the masters/servers
+					conn.send('Master Hbeat UPDATED')
+				except:
+					print 'Error occured'
+					conn.send('Master hbeat update FAILED')	
+				pass
+		elif msg.find("server") is not -1:
+			if msg.find('1:JOIN') is not -1:
+				new_ip = msg[msg.rfind(':') + 1:]   # finding the ip from the message received
+				print new_ip
+				try:
+					stor.add_new_server(new_ip)     # new server added
+					conn.send('Peer ADDED')
+				except:
+					print 'Error occured'
+					conn.send('Peer addition FAILED')				
+			elif msg.find('2:HBEAT') is not -1:     
+				new_ip = msg[msg.rfind(':') + 1:]
+				try:
+					stor.add_heartbeat_server(new_ip)      # updating the time when last ping came from the masters/servers
+					conn.send('Peer hbeat UPDATED')
+				except:
+					print 'Error occured'
+					conn.send('Peer Hbeat updation FAILED')	
+				pass
+
+
+		# add above functions for server also
+		# to add the function for updating the load of ther servers
+
 
 if __name__ == "__main__":
 	start_listening()
