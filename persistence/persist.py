5d4aa35f1dbc63cdaf6cd68abd9b67e64411b40c
Kritika4sharma <ks4347630@gmail.com>
2017-06-14 01:11:41 +0530

    master and server allocation to client is done

--- persistence/persist.py
+++ persistence/persist.py
@@ -1,24 +1,47 @@
 import socket
 from Storage import Storage
+import client_persist as cp
 import sqlite3 as db
 
 def start_listening():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	host = socket.gethostname()               # Get local machine name   
-	s.bind(('172.17.23.17', 11114))           # for same machine
+	#s.bind(('172.17.23.17', 11114))           # for same machine
+	#s.bind(('10.0.0.4',11114))
+	#port_mapper_obj = pm.PortMap()
+	#port = port_mapper_obj.get_port()
+	s.bind(('172.26.35.147',11122))
 	s.listen(10)
 
 	stor = Storage()                          # database will be created only once
 
 	while True:
+		stor.add_new_master('162.45.6.1')
+		stor.add_new_master('162.45.6.2')
+		stor.add_new_master('162.45.6.3')
+		stor.add_new_master('162.45.6.4')
+
+		stor.add_new_server('172.31.1.2')
+		stor.add_load_server('172.31.1.2',4)
+		stor.add_new_server('172.31.1.1')
+		stor.add_load_server('172.31.1.1',2)
+		stor.add_new_server('172.31.1.3')
+		stor.add_load_server('172.31.1.3',8)
+		stor.add_new_server('172.31.1.4')
+		stor.add_load_server('172.31.1.4',1)
+		stor.add_new_server('172.31.1.5')
+		stor.add_load_server('172.31.1.5',3)
+
 		conn, addr = s.accept()
 		msg = conn.recv(1024)
 		print msg
 		new_ip = addr[0]                 # ip from the addr recieved after connection of master/server
 		if msg.find("master") is not -1:
+			#print "master detected"
 			if msg.find('1:JOIN') is not -1:
 			#	new_ip = msg[msg.rfind(':') + 1:]   # finding the ip from the message received
 				master_id = msg[msg.rfind(':') + 1:]		#recieved master_id from the master
+				#print "here ",msg
 				try:
 					stor.add_new_master(new_ip)     # new server added
 			#		conn.send('Master ADDED')
@@ -53,10 +76,40 @@ def start_listening():
 				except:
 					print 'Error occured'
 					conn.send('Peer Hbeat updation FAILED')	
+			elif msg.find('2:LIST_OF_MASTERS') is not -1:     
+				try:
+					lis = stor.get_list_of_masters()      # updating the time when last ping came from the masters/servers
+					conn.send(lis)
+				except:
+					print 'Error occured'
+					conn.send('list of masters sending FAILED')	
 				pass
-
+		elif msg.find("client") is not -1:
+			print "client detected"
+			print "Creating dummy table :) -----------------"
+			
+			if msg.find('1:MASTER') is not -1:
+				#client_ip = msg[msg.rfind(':') + 1:]       #recieved  client_ip from the server
+				try:
+					#master_ip =  "heuuooo"
+					master_ip = stor.get_master()
+					#print "got ip",master_ip
+					conn.send('Master ip:'+master_ip)
+				except:
+					print 'Error occured'
+					conn.send('Master allocation failed')
+			elif msg.find('2:SERVER') is not -1:
+				#client_ip = msg[msg.rfind(':') + 1:]       #recieved  client_ip from the server
+				try:
+					server_ip = stor.get_server()
+					conn.send('Server ip:'+server_ip)
+				except:
+					print 'Error occured'
+					conn.send('Server allocation failed')
+				pass
 		# to add the function for updating the load of ther servers
 
 
 if __name__ == "__main__":
 	start_listening()
+
